4112738fc8fc7d2f7d0d75d110345f0c3dccded0
SelDanilEv <47692381+SelDanilEv@users.noreply.github.com>
2020-12-14 01:38:15 +0300

    Backup IUserRepositor

--- src/main/java/com/defender/test/Exceptions/ChampionshipNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.defender.test.Exceptions;
-
-public class ChampionshipNotFoundException extends Exception {
-    public ChampionshipNotFoundException(String message) {
-        super(message);
-    }
-}
